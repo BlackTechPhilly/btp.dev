119e9fd071db0d04ebec608831399191f3fc5c64
Ryan Small <rjsmall90@Ryans-MacBook-Pro.local>
2020-05-13 21:56:43 -0400

    react-app created, components added

---
+++ btp_site/src/component/contact/contactForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'; 
+
+export class ContactForm extends Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default ContactForm; 
---
+++ btp_site/src/component/home/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'; 
+
+
+export class Home extends Component {
+
+
+    //header
+
+    //navigation bar
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Home; 
