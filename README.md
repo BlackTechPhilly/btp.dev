ac02e8464fb07dab32c0cba7756e96111182a64f
Ian Kimble <ian.kimble@eRT.com>
2020-05-16 19:48:38 -0400

    nav and sponsor and member form

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # btp.dev
-website for Black Tech Philly 
+
+website for Black Tech Philly
+
+To run front end in terminal type "npm start"
---
+++ client/src/components/pages/Mission.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+
+const Mission = () => (
+  <div>
+    <Jumbotron fluid>
+      <Container>
+        <h1>Mission</h1>
+      </Container>
+    </Jumbotron>
+  </div>
+);
+
+export default Mission;
