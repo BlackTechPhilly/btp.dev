18c020f5ac5803916ab4d3bb85f6b52d65158f38
Ian Kimble <ian.kimble@eRT.com>
2020-05-17 16:14:19 -0400

    structured server folder

--- client/src/components/pages/Connect.js
+++ client/src/components/pages/Connect.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import { Tab, Tabs } from "react-bootstrap";
 import MemberForm from "./forms/MemberForm";
 import SponsorForm from "./forms/SponsorForm";
+import style from "../styles/formStyle.module.css";
 
 class Connect extends Component {
   render() {
     return (
       <Tabs id="uncontrolled-tab-example">
-        <Tab eventKey="Members" title="Become a Member">
+        <Tab
+          eventKey="onSelect"
+          title="Become a Member"
+          tabClassName={style.member}
+          onSelect
+        >
           <MemberForm />
         </Tab>
 
-        <Tab eventKey="Sponsors" title="Become a Sponsor">
+        <Tab
+          eventKey="Sponsors"
+          title="Become a Sponsor"
+          tabClassName="sponsor"
+        >
           <SponsorForm />
         </Tab>
       </Tabs>
