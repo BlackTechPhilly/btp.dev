4da2a150dbd8bb48d6f502e8ab7c07094000609e
Ryan Small <rjsmall90@Ryans-MacBook-Pro.local>
2020-08-27 00:12:40 -0400

    adding icon

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -7,6 +7,8 @@ import Home from "./components/pages/Home";
 import Mission from "./components/pages/Mission";
 import CodeOfConduct from "./components/pages/CodeOfConduct";
 import Connect from "./components/pages/Connect";
+import SingleBlog from "./components/pages/blogs/singleBlogPost";
+
 
 import Content from "./components/pages/content";
 
@@ -17,6 +19,9 @@ const AppRouter = () => (
     <Route exact path="/mission" component={Mission} />
     <Route exact path="/codeofconduct" component={CodeOfConduct} />
     <Route exact path="/connect" component={Connect} />
+    <Route exact path="/single_blog/:blogId" component={SingleBlog}/>
+
+
 
     {/* Test route for JSON*/}
     <Route exact path="/content" component={Content} />
