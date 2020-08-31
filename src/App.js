018be1eeeb30f6af2fe033f30e823c75622f537d
Ian Kimble <ian.kimble@eRT.com>
2020-08-30 22:52:24 -0400

    remove comment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AppRouter from "./AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/utilities/Footer";
 
-// changes
 const App = () => (
   <div style={{ fontFamily: "DINCondensed-Bold" }}>
     <BrowserRouter>
