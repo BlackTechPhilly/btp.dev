ebdbb0a09927b57229b686141e90d8c5ea2ad427
Ryan Small <rjsmall90@Ryans-MacBook-Pro.local>
2020-09-07 13:01:15 -0400

    loading component completed

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -7,18 +7,24 @@ import Home from "./components/pages/Home";
 import Mission from "./components/pages/Mission";
 import CodeOfConduct from "./components/pages/CodeOfConduct";
 import Connect from "./components/pages/Connect";
-import SingleBlog from "./components/pages/blogs/Item";
+import SingleBlog from "./components/pages/blogs/singleBlogPost";
+
+
+import Content from "./components/pages/content";
 
 const AppRouter = () => (
   <div>
     <NavBar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/item/:blogId" component={SingleBlog} />
-      <Route exact path="/mission" component={Mission} />
-      <Route exact path="/codeofconduct" component={CodeOfConduct} />
-      <Route exact path="/connect" component={Connect} />
-    </Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/mission" component={Mission} />
+    <Route exact path="/codeofconduct" component={CodeOfConduct} />
+    <Route exact path="/connect" component={Connect} />
+    <Route exact path="/single_blog/:blogId" render={() => <SingleBlog /> }/>
+
+
+
+    {/* Test route for JSON*/}
+    <Route exact path="/content" component={Content} />
   </div>
 );
 
