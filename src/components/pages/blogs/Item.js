9090e41d217926d132a1fb45f73eec806eec88ff
Ryan Small <rjsmall90@Ryans-MacBook-Pro.local>
2020-09-27 12:43:02 -0400

    conditional statment added

--- src/components/pages/blogs/Item.js
+++ src/components/pages/blogs/Item.js
@@ -5,9 +5,6 @@ import { useParams } from "react-router-dom";
 import reactStringReplace from "react-string-replace";
 import ReactPlayer from "react-player";
 
-function ShowVideo(props){
-  if ('video' in props === false ) document.getElementById("vid").style.visibility = "hidden"
-}
 
 const Item = () => {
   const blog = useParams();
@@ -20,9 +17,7 @@ const Item = () => {
       .then((response) => {
         const content = response.data;
         setBlog(content);
-        ShowVideo(content);
         console.log('video' in content)
- 
       });
   }, [blogId, setBlog]);
 
@@ -30,6 +25,7 @@ const Item = () => {
 
   console.log(singleBlog.video)
 
+if ( 'video' || 'picture' in singleBlog === false ) {
   return (
     <div style={{}}>
       <Jumbotron>
@@ -43,12 +39,6 @@ const Item = () => {
               </i>
             </>
           </div>
-          <Row>
-            <div  id="vid">
-                {/* create ternary operator for if there is no video*/}
-                <ReactPlayer url={singleBlog.video} /> 
-            </div>  
-          </Row>
           <Row>
             <Col>
               <p
@@ -66,7 +56,43 @@ const Item = () => {
         </Container>
       </Jumbotron>
     </div>
-  );
+  );}
+  else return (
+    <div style={{}}>
+    <Jumbotron>
+      <Container style={{ textAlign: "center" }}>
+        <div>
+          <h1 style={{ fontSize: "50px" }}>{singleBlog.title} </h1>
+          <h3> {singleBlog.subtitle} </h3>
+          <>
+            <i>
+              Written By: {singleBlog.author} on {created}
+            </i>
+          </>
+        </div>
+        <Row>
+          <div id="vid">
+              <ReactPlayer url={singleBlog.video} /> 
+          </div>  
+        </Row>
+        <Row>
+          <Col>
+            <p
+              style={{
+                fontFamily: "Eina01-Regular",
+                fontSize: "15px",
+                padding: "4px",
+                textAlign: "center",
+              }}
+            >
+              {reactStringReplace(singleBlog.body,/(\^)/g,(match, i) => (<p key={i}>{match}</p>))}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  </div>
+  )
 };
 
 export default Item;
