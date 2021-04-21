f2d1cd8256f7c08f3b41f4a0746218b74194f84c
Ian Kimble <ian.kimble@eRT.com>
2021-04-20 21:11:52 -0400

    toml

--- next.config.js
+++ next.config.js
@@ -1,33 +1,42 @@
 // Overriding Next default setting to use custom CSS/ Bootstrap
-const withCSS = require("@zeit/next-css");
+const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-  }],
+	[
+		optimizedImages,
+		{
+			/* config for next-optimized-images */
+		}
+	]
 
-  // your other plugins here
-
+	// your other plugins here
 ]);
 
-module.exports = withCSS({
-  // cssModules: true,
-});
+module.exports = withCSS(
+	{
+		// cssModules: true,
+	}
+);
 
 // Configuration/ ENV file for Next.js
 module.exports = {
-  publicRuntimeConfig: {
-    APP_NAME: "BTP NEXT",
-    API_DEVELOPMENT: "http://localhost:5050",
-    PRODUCTION: false,
-  },
+	publicRuntimeConfig: {
+		APP_NAME: 'BTP NEXT',
+		API_DEVELOPMENT: 'http://localhost:5050',
+		PRODUCTION: false
+	}
 };
 
 module.exports = {
-  BTP_BLOG: "https://btpblog.herokuapp.com/tech-posts",
-  future: {
-    webpack5: true,
-  },
-  SENDGRID: "",
+	BTP_BLOG: 'https://btpblog.herokuapp.com/tech-posts',
+	future: {
+		webpack5: true
+	},
+	SENDGRID: ''
+};
+
+module.exports = {
+	// Target must be serverless
+	target: 'serverless'
 };
